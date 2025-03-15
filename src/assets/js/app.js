61975c31b2bfb8b8883e26e1fb2c81dbf265c9c7
Yarlen <2020030817@upsin.edu.mx>
2025-03-15 16:34:50 -0700

    agregamos carpeta de js

---
+++ src/assets/js/app.js
@@ -0,0 +1,5 @@
+document.getElementById("app-title"); // Para seleccionar un elemento de id
+document.querySelector("#app-title"); // Esta es la forma de la que nosotros seleccionamos o podemos tener acceso a elementos que son unicos
+document.querySelector("p"); // Para que me traiga una etiqueta de elemento p, solo me traera un solo elemento, no todos
+document.querySelectorAll("p"); // De esta forma me traera todas las etiquetas de elemento p
+document.getElementsByClassName("menu-items"); // Le estamos pidiendo que nos traiga todos los elementos con el n ombre de la clase menu items
