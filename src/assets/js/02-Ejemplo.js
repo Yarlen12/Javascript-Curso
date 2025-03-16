e4293bb31b3121e070114cf4f2018697fe499adf
Yarlen <2020030817@upsin.edu.mx>
2025-03-15 21:19:44 -0700

    Terminamos el ejemplo de como cambiar un valor de una variable por js

---
+++ src/assets/js/02-Ejemplo.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("input"); // Esto hace que el selector traiga al input
+console.log(input);
+
+console.dir(input); // nos permite inspeccionar el objeto 'input', Es un dirigeme al nodo, nos va a traer el objeto
+// Esto nos muestra todas las propiedades y métodos del nodo DOM,
+// permitiéndonos acceder y manipularlas con JavaScript.
+
+input.value; // Es para que nos traiga el valor de value
+input.value = "Apellido"; // Asi podemos cambiar el valor de la variable en consola
